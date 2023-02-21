39e661f7c35d4e721a42f743069b51095d47ca5a
Supriya Reddy <121212953+Supriya-wl@users.noreply.github.com>
2023-02-21 14:21:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-//your code here
+//your code herevar todoValue = document.getElementById("newTodoInput").value;
+var btn = document.getElementById("addTodoBtn");
+var ol = document.getElementById("todoList");
+
+function addTodo() {
+  if (todoValue) {
+    // we need to create a li
+    var li = document.createElement("li");
+    li.textContent = todoValue;
+    ol.appendChild(li);
+  }
+}
+
+btn.addEventListener("click", addTodo);
 
